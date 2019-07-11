437156e8087bc007020cf026e71c34b82d7d2724
Jacob Aulenback <jacobaulenback@gmail.com>
2019-07-10 19:01:30 -0700

    Add outline for Profile

---
+++ profile/__main__.py
@@ -0,0 +1,59 @@
+from concurrent import futures
+from time import sleep
+import grpc
+
+from user_pb2 import User, UserInfo
+from wall_pb2 import WallQuery
+from posts_pb2 import Posts
+
+from profile_pb2_grpc import add_ProfileServiceServicer_to_server, ProfileServiceServicer
+
+from wall_pb2_grpc import WallServiceStub
+from posts_pb2_grpc import PostsServiceStub
+
+
+class ProfileService(ProfileServiceServicer):
+    
+    def __init__(self) -> None:
+        
+        self.wall_channel = grpc.insecure_channel('wall:6969')
+        self.wall_stub = WallServiceStub(self.wall_channel)
+
+        try:
+            grpc.channel_ready_future(self.wall_channel).result(timeout = 20)
+        except grpc.FutureTimeoutError:
+            print("Failed to connect to wall")
+
+        self.posts_channel = grpc.insecure_channel('posts:2884')
+        self.posts_stub = PostsServiceStub(self.posts_channel)
+        try:
+            grpc.channel_ready_future(self.posts_channel).result(timeout = 20)
+        except grpc.FutureTimeoutError:
+            print("Failed to connect to posts")
+
+
+    def get_friends(self, request: User, context: grpc.RpcContext = None) -> UserInfo:
+        
+        #Is this info from User Settings? 
+        return UserInfo()
+
+
+    def get_wall(self, request: WallQuery, context: grpc.RpcContext = None) -> Iterator[Posts]:
+        
+        #Right now profile just passes along from wall which just passes along from posts which just does an SQL query.
+        #That is to say this is not production ready
+        
+        wall_posts = self.wall_stub.fetch(request)
+        for post in wall_posts:
+            yield post
+
+
+if __name__ == "__main__":
+    profile_port = 2885
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers = 4))
+    add_ProfileServiceServicer_to_server(servicer = ProfileService(), server = server)
+    server.add_insecure_port('[::]:{0}'.format(profile_port))
+    print("Starting Friends Server")
+    server.start()
+    while True:
+        sleep(1000)
