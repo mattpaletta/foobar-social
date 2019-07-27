8c6e135bb1a0446c8a90f77b2b5f3447dbd4cf79
Matthew Paletta <mattpaletta@gmail.com>
2019-07-26 20:27:18 -0700

    Started adding post_importer impl and redis wrapper

--- post_importer_cpp/include/main.cpp
+++ post_importer_cpp/include/main.cpp
@@ -5,26 +5,71 @@
 #include <grpcpp/grpcpp.h>
 #include <iostream>
 #include <memory>
+#include <cstdlib>
 
 //#include <redox/redox.hpp>
 #include "cppkafka/producer.h"
 
 #include "post_importer.grpc.pb.h"
+#include "redox_helper.h"
 
 class PostImporterService final : public foobar::post_importer::PostImporterService::Service {
 public:
-    PostImporterService() {
-
+    PostImporterService() : rdx("post_importer_redis"),
+            IMPORT_QUEUE(std::getenv("IMPORT_QUEUE")),
+            POST_INCREMENT_KEY(std::getenv("POST_INCREMENT_KEY")) {
+        if (POST_INCREMENT_KEY == nullptr || IMPORT_QUEUE == nullptr) {
+            throw std::runtime_error("Failed to read env variables");
+        }
     }
 
     grpc::Status create_post(::grpc::ServerContext *context, const ::foobar::posts::Post *request,
                              ::foobar::shared::Empty *response) override {
 
-        return Service::create_post(context, request, response);
+        auto has_msg = !request->msg().empty();
+        auto has_username = !request->username().empty();
+        auto has_location = request->has_loc() && PostImporterService::is_valid_loc(request->loc());
+
+        if (!has_msg) {
+            return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Missing message");
+        }
+
+        if (!has_username) {
+            return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Missing username");
+        }
+
+        if (!has_location) {
+            return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "Invalid Location");
+        }
+
+        std::size_t next_index = this->rdx.incr(this->POST_INCREMENT_KEY);
+
+        // Make a copy of request, so we can mutate it.
+        foobar::posts::Post post = *request;
+
+        // Add in the epoch time (according to the server)
+        const auto now = std::chrono::system_clock::now();
+        const auto epoch = now.time_since_epoch();
+        const auto seconds = std::chrono::duration_cast<std::chrono::seconds>(epoch);
+        post.set_datetime(seconds.count());
+
+        
+
+        return grpc::Status::OK;
     }
 
 private:
+    const char* IMPORT_QUEUE;
+    const char* POST_INCREMENT_KEY;
 
+    Redis rdx;
+
+    static bool is_valid_loc(const foobar::shared::Location& loc) {
+        return  loc.lat() > -90 &&
+                loc.lat() < 90 &&
+                loc.long_() > -180 &&
+                loc.long_() < 180;
+    }
 };
 
 int main(int argc, char* argv[]) {
---
+++ post_importer_cpp/include/redox_helper.h
@@ -0,0 +1,107 @@
+//
+// Created by Matthew Paletta on 2019-07-26.
+//
+
+#ifndef REDOX_HELPER_H
+#define REDOX_HELPER_H
+#include <string>
+#include <iostream>
+#include <sstream>
+
+#include "redox.hpp"
+
+
+// https://github.com/IBM-Swift/Kitura-redis/blob/master/Sources/SwiftRedis/Redis%2BBasic.swift
+
+class Redis {
+public:
+    Redis(const std::string& host, const int& port = 6379) : rdx() {
+        // Does not sleep the event loop
+        // Better performance, higher CPU
+        this->rdx.noWait(true);
+
+        if (this->rdx.connect(host, port)) {
+            throw std::runtime_error("Unable to connect to redis host");
+        }
+    }
+
+    std::string get(const std::string& key) {
+        return this->rdx.get(key);
+    }
+
+    bool set(const std::string& key, const std::string& value) {
+        return this->rdx.set(key, value);
+    }
+
+    bool del(const std::string& key) {
+        return this->rdx.del(key);
+    }
+
+    std::size_t incr(const std::string& key, const std::size_t amount = 1) {
+        const auto& string = this->run_wait_cmd({"INCRBY", key, std::to_string(amount)});
+        std::stringstream sstream(string);
+        std::size_t result;
+        sstream >> result;
+        return result;
+    }
+
+    std::string blpop(const std::string& key, const std::size_t& timeout = 0) {
+        return this->block_helper("BLPOP", key, timeout);
+    }
+
+    std::string brpop(const std::string& key, const std::size_t& timeout = 0) {
+        return this->block_helper("BRPOP", key, timeout);
+    }
+
+    std::string brpoplpush(const std::string& src, const std::string& dest, const std::size_t& timeout = 0) {
+        return this->run_wait_cmd({"BRPOPLPUSH", src, dest, std::to_string(timeout)});
+    }
+
+    void lpush(const std::string& key, const std::string& value) {
+        auto& cmd = this->rdx.commandSync<std::string>({"LPUSH", key, value});
+        cmd.free();
+    }
+
+    void lpush(const std::string& key, const std::vector<std::string>& values) {
+        std::vector<std::string> vec;
+        vec.emplace_back("LPUSH");
+        vec.push_back(key);
+        vec.insert(vec.end(), values.begin(), values.end());
+        auto& cmd = this->rdx.commandSync<std::string>(std::move(vec));
+        cmd.free();
+    }
+
+    void rpush(const std::string& key, const std::string& value) {
+        auto& cmd = this->rdx.commandSync<std::string>({"RPUSH", key, value});
+        cmd.free();
+    }
+
+    void rpush(const std::string& key, const std::vector<std::string>& values) {
+        std::vector<std::string> vec;
+        vec.emplace_back("RPUSH");
+        vec.push_back(key);
+        vec.insert(vec.end(), values.begin(), values.end());
+        auto& cmd = this->rdx.commandSync<std::string>(std::move(vec));
+        cmd.free();
+    }
+
+private:
+    redox::Redox rdx;
+
+    std::string block_helper(const std::string& redis_cmd, const std::string& key, const std::size_t& timeout = 0) {
+        return this->run_wait_cmd({redis_cmd, key, std::to_string(timeout)});
+    }
+
+    std::string run_wait_cmd(const std::vector<std::string>& redis_cmd) {
+        auto& cmd = this->rdx.commandSync<std::string>(redis_cmd);
+        auto reply = cmd.reply();
+        cmd.free();
+        return reply;
+    }
+};
+
+//Redis getClient(const std::string& host, const int& port) {
+//    return Redis(host, port);
+//}
+
+#endif
